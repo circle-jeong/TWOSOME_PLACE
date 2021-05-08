af58d4f1cfd5b1fcbc96c9dfed3f46e1bc43761f
circle-jeong <kimchubby92@gmail.com>
2021-05-08 21:18:13 +0900

    twosome

---
+++ twosome-d/js/custopm-m.js
@@ -0,0 +1,73 @@
+/* $( function () {
+  var $bar = $('.navi');
+  var $gnb = $('.gnb');
+  var $close = $('.x > a');
+
+  $gnb.css('top', '-100%')
+
+  $bar.click( function ( e ) {
+    e.preventDefault();
+    $gnb.stop().animate({
+      top: 0,
+      display: 'block',
+      opacity: '1'
+    }, 600);
+  } );
+  $close.click( function ( e ) {
+    e.preventDefault();
+    $gnb.stop().animate({
+      top: '-100%',
+      display: 'none',
+      opacity: '0'
+    }, 600);
+  } );
+
+} ); */
+
+$( document ).ready( function () {
+
+
+  /* 햄버거 아이콘 클릭 이벤트 */
+
+  $('.navi').click( function (e) {
+    e.preventDefault();
+    $('.gnb').addClass('on');
+  } );
+  $('.x > a').click( function (e) {
+    e.preventDefault();
+    $('.gnb').removeClass('on');
+    setTimeout( function () {
+      $('.sub').slideUp();
+      $('.gnbb>li>a>i').removeClass('fa-minus').addClass('fa-plus');
+    },300)
+  } );
+
+
+
+  /* 메뉴 클릭 이벤트 */
+
+  $('.gnbb>li>a').click(function () {
+
+    var check = $(this).hasClass('on');
+    var minus = 'fa-minus'
+    var plus = 'fa-plus'
+    var menu = $('.gnbb>li>a')
+    var sub = $('.gnbb>li>ul')
+
+    if(check){
+      $(this).removeClass('on');
+      $(this).siblings('ul').stop().slideUp(300);
+      $(this).find('i').removeClass(minus).addClass(plus);
+   } else {
+      menu.removeClass('on');
+      menu.find('i').removeClass(minus).addClass(plus)
+      sub.stop().slideUp(300);
+      $(this).addClass('on');
+      $(this).siblings('ul').stop().slideDown(300);
+      $(this).find('i').removeClass(plus).addClass(minus);
+   }
+  } );
+
+} );
+
+
---
+++ twosome-m/js/custom.js
@@ -0,0 +1,44 @@
+$ ( function () {
+  var $bar = $('.navi');
+  var $gnb = $('.gnb');
+  var $close = $('.x > a')
+
+  $gnb.css('top', '-100%')
+
+  $bar.click( function ( e ) {
+    e.preventDefault();
+    $gnb.stop().animate({
+      top: 0,
+      display: 'block',
+      opacity: '1'
+    }, 600);
+  } );
+  $close.click( function ( e ) {
+    e.preventDefault();
+    $gnb.stop().animate({
+      top: '-100%',
+      display: 'none',
+      opacity: '0'
+    }, 600);
+  } );
+
+  var nav = $('header');
+  var logo = $('#logo');
+  var navi = $('.navi')
+
+  $(window).scroll( function () {
+    if( $(this).scrollTop() > 136 ){
+      nav.addClass('hdfixed');
+      nav.css('backgroundColor', '#f0e9e3f8');
+      logo.addClass('ls');
+      navi.addClass('bar');
+
+    } else {
+      nav.removeClass('hdfixed');
+      nav.css('backgroundColor', 'transparent');
+      logo.removeClass('ls');
+    }
+  } );
+    //스크롤 이벤트
+    AOS.init();
+} );
